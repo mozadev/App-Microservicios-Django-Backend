a1a3bd2a700c6e83b9b76131aad7f56ca00d716b
ceosmore <ceosmore@gmail.com>
2024-04-20 17:05:32 -0500

    feat: :sparkles: Config blog-apps-posts-models.py

--- blog/apps/posts/models.py
+++ blog/apps/posts/models.py
@@ -6,16 +6,17 @@ import uuid
 class Author(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=100)
-    display_name = models.CharField(max_length=100, blank=True, null=True)
+    picture = models.ImageField(default='media/users/user_default_profile.png',  upload_to='media/users/pictures/', blank=True, null=True, verbose_name='Picture')
     
     def __str__(self):
         return self.username
     
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     
     def __str__(self):
