cc298181de540a137300c9c79b751aaa95f02a5f
ceosmore <ceosmore@gmail.com>
2024-04-20 12:35:23 -0500

    feat: :sparkles: Add path apps.category.urls to urlpatterns on blog-core-urls.py

--- blog/core/urls.py
+++ blog/core/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/posts/', include('apps.posts.urls')),
+    path('api/category/', include('apps.category.urls')),
+    
     path('admin/', admin.site.urls),
     
     
