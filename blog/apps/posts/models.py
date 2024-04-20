d013a73e83884805c2de869b82216247fc2c25e9
ceosmore <ceosmore@gmail.com>
2024-04-20 13:00:03 -0500

    feat: :sparkles: Add Author and Post on blog-apps-posts-models.py

--- blog/apps/posts/models.py
+++ blog/apps/posts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=100)
+    display_name = models.CharField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return self.username
+    
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
