ee9a6137db43c1ae4fc05593545f538f6e2ff293
ceosmore <ceosmore@gmail.com>
2024-04-22 18:43:00 -0500

    refactor: :art: Refactor settings auth and blog

--- blog/core/settings.py
+++ blog/core/settings.py
@@ -223,31 +223,31 @@ if not DEBUG:
     
 
 
-#DJANGO-CREDITOR NO FUNCIONA con s3 atravez  django storages sin this line in setting.py
+    #DJANGO-CREDITOR NO FUNCIONA con s3 atravez  django storages sin this line in setting.py
 
-AWS_QUERYTRING_AUTH = False
+    AWS_QUERYTRING_AUTH = False
 
-#aws setting
+    #aws setting
 
-AWS_ACCESS_KEY_ID=env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY=env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+    AWS_ACCESS_KEY_ID=env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY=env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.us-east-2.amazonaws.com'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.us-east-2.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_DEFAULT_ACL = 'public-read'
 
-#S3 STATIC SETTINGS
-STATIC_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-STATICFILES_STORAGE = 'core.storage_backends.s3bo.StaticStorage'
+    #S3 STATIC SETTINGS
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'core.storage_backends.s3bo.StaticStorage'
 
-#s3 public media settings
-PUBLIC_MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'core.storage_backends.s3bo.MediaStorage'
+    #s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'core.storage_backends.s3bo.MediaStorage'
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static'),)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
