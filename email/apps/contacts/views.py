ac7b5c99903497479d6323874e6afa7e34db2569
ceosmore <ceosmore@gmail.com>
2024-04-22 21:45:44 -0500

    feat: :sparkles: Add views.py on email-apps-contacts

--- email/apps/contacts/views.py
+++ email/apps/contacts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework_api.views import StandardAPIView
+from rest_framework import permissions, status
+from django.core.mail import send_mail
+from .models import Contact
+from .serializers import ContactSerializer
 
-# Create your views here.
+class ContactCreateView(StandardAPIView):
+    def post(self, request, format=None):
+        serializer = ContactSerializer(data=request.data)
+
+        if serializer.is_valid():
+            contact = serializer.save()
+
+            message_body = f"""
+                            Name: {contact.name}
+                            Email: {contact.email}
+                            Phone: {contact.phone}
+                            Budget: {contact.budget}
+
+                            Message:
+                            {contact.message}
+                            """
+
+            send_mail(
+                contact.subject,
+                message_body,
+                'mail@solopython.com',
+                ['mail@solopython.com'],
+                fail_silently=False
+            )
+
+            return self.send_response('Message sent successfully')
+
+        return self.send_error(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
